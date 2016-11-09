01c8c7a096c616918d5f4c0ee7f5a413dfe1dbef
Marko Koprivnjak <koprivnjak.marko@hotmail.com>
2016-11-09 20:56:13 +0100

    Dodan UI za registraciju

---
+++ BeFit/BeFit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeFit
+//
+//  Created by MTLab on 09/11/2016.
+//  Copyright © 2016 Kristijan Gašljević. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
