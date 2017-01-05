0db34611677e4b6af72b473dc7f0070b7519faeb
Marko Koprivnjak <koprivnjak.marko@hotmail.com>
2017-01-05 01:19:12 +0100

    JSON parsiranje podataka o korisnikovim obrocima

--- BeFit/BeFit/DiaryTableViewController.swift
+++ BeFit/BeFit/DiaryTableViewController.swift
@@ -9,11 +9,60 @@
 import UIKit
 
 class DiaryTableViewController: UITableViewController {
+    
+    
+    var sections = ["Meals", "Exercises"];
+    var meals = [String]();
+    var caloriesMeals = [String]();
+    var exercises = ["trčanje", "plivanje", "trbušnjaci", "sklekovi", "nogomet"];
+    var caloriesExercises = ["150", "300", "100", "120", "450"];
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
+        
+        //get users meals
+        var request = URLRequest(url: URL(string: "http://befitapp.esy.es/foods/getfoodrecordbyuser.php")!);
+        request.httpMethod = "POST";
+        
+        let postString = "userid=\(user.userID)";
+        request.httpBody = postString.data(using: .utf8)
+        
+        let task = URLSession.shared.dataTask(with: request)
+        { data, response, error in
+            
+            //response form the server
+            let responseString = String(data: data!, encoding: .utf8)
+            print("responseString = \(responseString!)")
+            
+            //parsing server response to JSON
+            do {
+                
+                let parsedData = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! [String:AnyObject]
+                
+                //print (parsedData)
+                
+                //meals status end description
+                let mealsStatus = parsedData["status"] as! String
+                print("parsedData - status: = \(mealsStatus)")
+                
+                
+                if(mealsStatus=="success"){
+                    if let mealsList = parsedData["description"] {
+                        for index in 0...mealsList.count {
+                            let aObject = mealsList[index] as! [String :AnyObject]
+                            
+                            self.meals.append(aObject["foodName"] as! String);
+                            self.caloriesMeals.append(aObject["cal"] as! String);
+                        }
+                    }
+                }
+                
+                
+            } catch let error as NSError {
+                print(error)
+            }
+        }
+        task.resume();        // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
@@ -27,25 +76,43 @@ class DiaryTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        // return the title for each section
+        return self.sections[section];
+    }
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        // return the number of sections
+        return self.sections.count;
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        // return the number of rows for each section
+        if section == 0 {
+            return self.meals.count;
+        }
+        else {
+            return self.exercises.count;
+        }
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
 
         // Configure the cell...
+        if indexPath.section == 0 {
+            cell.textLabel?.text = meals[indexPath.row];
+            cell.detailTextLabel?.text = caloriesMeals[indexPath.row];
+        }
+        else {
+            cell.textLabel?.text = exercises[indexPath.row];
+            cell.detailTextLabel?.text = caloriesExercises[indexPath.row];
+        }
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
